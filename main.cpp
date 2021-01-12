c76ad3161f3a76ad8b2053ea4779e34093829f8a
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-01-12 20:32:15 +0100

    attempt to use u32string (#12)

--- main.cpp
+++ main.cpp
@@ -4,33 +4,35 @@
 #include <algorithm>
 #include <locale>
 
-const std::string WHITESPACE = " \n\r\t\f\v";
+std::u32string const WHITESPACE = U" \n\r\t\f\v";
 
-std::string ltrim(const std::string& s)
+std::u32string ltrim(const std::u32string& s)
 {
     size_t start = s.find_first_not_of(WHITESPACE);
-    return (start == std::string::npos) ? "" : s.substr(start);
+    return (start == std::u32string::npos) ? U"" : s.substr(start);
 }
  
-std::string rtrim(const std::string& s)
+std::u32string rtrim(const std::u32string& s)
 {
     size_t end = s.find_last_not_of(WHITESPACE);
-    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+    return (end == std::u32string::npos) ? U"" : s.substr(0, end + 1);
 }
  
-std::string trim(const std::string& s)
+std::u32string trim(const std::u32string& s)
 {
     return rtrim(ltrim(s));
 }
 
-int detect_entry_type(std::string entry){
+std::u32string sanitize_entry(){}
+
+int detect_entry_type(std::u32string entry){
     // returns :
     // 1 if text
     // 2 if Morse code
     // 3 if file to open
     entry = trim(entry);
 
-    if (entry.find(".txt") != std::string::npos){
+    if (entry.find(U".txt") != std::u32string::npos){
         // then we have a (path to a) .txt file
         return 3;
     }
@@ -42,77 +44,95 @@ int detect_entry_type(std::string entry){
     return 1; 
 }
 
-std::map <std::string, std::string> create_dict_morse_to_car(){
-    std::map <std::string, std::string> m;
+std::map <std::u32string, std::u32string> create_dict_morse_to_car(){
+    std::map <std::u32string, std::u32string> m;
 
-    m[""] = "";
-    m[".-"] = "a";
-    m["-..."] = "b";
-    m["-.-."] = "c";
-    m["-.."] = "d";
-    m["."] = "e";
-    m["..-."] = "f";
-    m["--."] = "g";
-    m["...."] = "h";
-    m[".."] = "i";
-    m[".---"] = "j";
-    m["-.-"] = "k";
-    m[".-.."] = "l";
-    m["--"] = "m";
-    m["-."] = "n";
-    m["---"] = "o";
-    m[".--."] = "p";
-    m["--.-"] = "q";
-    m[".-."] = "r";
-    m["..."] = "s";
-    m["-"] = "t";
-    m["..-"] = "u";
-    m["...-"] = "v";
-    m[".--"] = "w";
-    m["-..-"] = "x";
-    m["-.--"] = "y";
-    m["--.."] = "z";
-    m["-----"] = "0";
-    m[".----"] = "1";
-    m["..---"] = "2";
-    m["...--"] = "3";
-    m["....-"] = "4";
-    m["....."] = "5";
-    m["-...."] = "6";
-    m["--..."] = "7";
-    m["---.."] = "8";
-    m["----."] = "9";
-    m[".-.-.-"] = ".";
-    m["--..--"] = ",";
-    m["..--.."] = "?";
-    m[".----."] = "'";
-    m["-.-.--"] = "!";
-    m["-..-."] = "/";
-    m["-.--."] = "(";
-    m["-.--.-"] = ")";
-    m["&"] = ".-...";
+    m[U""]         = U"";
+    m[U".-"]       = U"a";
+    m[U"-..."]     = U"b";
+    m[U"-.-."]     = U"c";
+    m[U"-.."]      = U"d";
+    m[U"."]        = U"e";
+    m[U"..-."]     = U"f";
+    m[U"--."]      = U"g";
+    m[U"...."]     = U"h";
+    m[U".."]       = U"i";
+    m[U".---"]     = U"j";
+    m[U"-.-"]      = U"k";
+    m[U".-.."]     = U"l";
+    m[U"--"]       = U"m";
+    m[U"-."]       = U"n";
+    m[U"---"]      = U"o";
+    m[U".--."]     = U"p";
+    m[U"--.-"]     = U"q";
+    m[U".-."]      = U"r";
+    m[U"..."]      = U"s";
+    m[U"-"]        = U"t";
+    m[U"..-"]      = U"u";
+    m[U"...-"]     = U"v";
+    m[U".--"]      = U"w";
+    m[U"-..-"]     = U"x";
+    m[U"-.--"]     = U"y";
+    m[U"--.."]     = U"z";
+    m[U"-----"]    = U"0";
+    m[U".----"]    = U"1";
+    m[U"..---"]    = U"2";
+    m[U"...--"]    = U"3";
+    m[U"....-"]    = U"4";
+    m[U"....."]    = U"5";
+    m[U"-...."]    = U"6";
+    m[U"--..."]    = U"7";
+    m[U"---.."]    = U"8";
+    m[U"----."]    = U"9";
+    m[U".-.-.-"]   = U".";
+    m[U"--..--"]   = U",";
+    m[U"..--.."]   = U"?";
+    m[U".----."]   = U"'";
+    m[U"-.-.--"]   = U"!";
+    m[U"-..-."]    = U"/";
+    m[U"-.--."]    = U"(";
+    m[U"-.--.-"]   = U")";
+    m[U".-..."]    = U"&";
+    m[U"---..."]   = U":";
+    m[U"-.-.-."]   = U";";
+    m[U"-...-"]    = U"=";
+    m[U".-.-."]    = U"+";
+    m[U"-....-"]   = U"-";
+    m[U"..--.-"]   = U"_";
+    m[U"...-..-"]  = U"$";
+    m[U".--.-."]   = U"@";
+    m[U".-.-"]     = U"æ";
+    m[U".--.-"]    = U"à";
+    m[U"-.-.."]    = U"ç";
+    m[U"..--."]    = U"ð";
+    m[U".-..-"]    = U"è";
+    m[U"..-.."]    = U"é";
+    m[U"--.--"]    = U"ñ";
+    m[U"---."]     = U"ø";
+    m[U"..--"]     = U"ü";
 
     return m;
 }
 
-std::map <std::string, std::string> create_dict_car_to_morse(){
-    std::map <std::string, std::string> mtc = create_dict_morse_to_car();
-    std::map <std::string, std::string> m;
+std::map <std::u32string, std::u32string> create_dict_car_to_morse(){
+    std::map <std::u32string, std::u32string> mtc = create_dict_morse_to_car();
+    std::map <std::u32string, std::u32string> m;
 
     for (auto item: mtc){
         // std::cout<<item.first<<" -> "<<item.second<<std::endl;
         m[item.second] = item.first;
     }
-    m[" "] = "   ";
+    m[U" "] = U"   ";
+    m[U"ù"] = U"..-";
 
     return m;
 }
 
 
-std::string Morse_to_text(std::string morse, 
-                        std::map <std::string, std::string> dict_morse_car = create_dict_morse_to_car()){
-    std::string res = "";
-    std::string temp = "";
+std::u32string Morse_to_text(std::u32string morse, 
+                        std::map <std::u32string, std::u32string> dict_morse_car = create_dict_morse_to_car()){
+    std::u32string res  = U"";
+    std::u32string temp = U"";
 
     // we first trim the leading and trailing spaces of morse text
     morse = trim(morse);
@@ -126,19 +146,19 @@ std::string Morse_to_text(std::string morse,
             if (i+1 < morse.length() and morse[i+1] != ' '){
                 // it's the end of a character
                 res += dict_morse_car[temp];
-                temp = "";
+                temp = U"";
             }
             else if (i>0 and morse[i-1] != ' '){
                 // it's the end of a character 
                 res += dict_morse_car[temp];
-                temp = "";
+                temp = U"";
             }
             if (i>0 and morse[i-1] == ' '){
                 // we're in the middle of a space -> do nothing
             }
             if (i>1 and morse[i-1] == ' ' and morse[i-2] == ' '){
                 // we're at the end of a space
-                res += " ";
+                res += U" ";
             }
         }
         else if (i+1 == morse.length()){
@@ -156,64 +176,67 @@ std::string Morse_to_text(std::string morse,
     return res;
 }
 
-std::string text_to_Morse(std::string text, 
-                        std::map <std::string, std::string> dict_car_morse = create_dict_car_to_morse()){
-    std::string res = "";
-    dict_car_morse[" "] = "  ";
+std::u32string text_to_Morse(std::u32string text, 
+                        std::map <std::u32string, std::u32string> dict_car_morse = create_dict_car_to_morse()){
+    std::u32string res = U"";
+    dict_car_morse[U" "] = U"  ";
     text = trim(text);
 
     std::locale loc;
 
-    for (std::string::size_type i=0; i<text.length(); i++){
+    for (std::u32string::size_type i=0; i<text.length(); i++){
         text[i] = std::tolower(text[i], loc);
     }
     
     for (int i = 0; i<text.length(); i++){
-        std::string car (1,text[i]);
-        std::map <std::string, std::string>::const_iterator pos = dict_car_morse.find(car);
+        std::u32string car (1,text[i]);
+        std::map <std::u32string, std::u32string>::const_iterator pos = dict_car_morse.find(car);
     
         res += pos->second;
-        res += " ";
+        res += U" ";
     }
 
     return res;
 }
 
 int main(int argc, char *argv[]){
-    std::string entry_text = "";
+    std::u32string entry_text = U"";
 
     if(argc > 1){
         
         for (int i = 1; i<argc; i++){
-            entry_text += std::string(argv[i]);
+            std::string temp(argv[i]);
+            std::u32string temp2;
+            temp2.assign(temp.begin(), temp.end());
+            entry_text += temp2;
             if (i + 1 <argc){
-                entry_text += " ";
+                entry_text += U" ";
             }
         }
-        std::cout<<std::endl<<entry_text<<std::endl;
+        std::cout<<std::endl<<entry_text.c_str()<<std::endl;
     }
 
-    std::map <std::string, std::string> dict_morse_car = create_dict_morse_to_car();
-    std::map <std::string, std::string> dict_car_morse = create_dict_car_to_morse();
+    std::map <std::u32string, std::u32string> dict_morse_car = create_dict_morse_to_car();
+    std::map <std::u32string, std::u32string> dict_car_morse = create_dict_car_to_morse();
    
 
     int entry_type = detect_entry_type(entry_text);
 
     if (entry_type == 1){
         // we have text -> Morse
-        std::cout<<text_to_Morse(entry_text, dict_car_morse)<<std::endl;
+        std::cout<<text_to_Morse(entry_text, dict_car_morse).c_str()<<std::endl;
     }
     else if (entry_type == 2){
         // we have Morse code
-        std::cout<<Morse_to_text(entry_text, dict_morse_car)<<std::endl;
+        std::cout<<Morse_to_text(entry_text, dict_morse_car).c_str()<<std::endl;
     }
     
     // various tests :
-
-    std::string test_morse_1 = "    .... . .-.. .-.. ---   .-- --- .-. .-.. -..   -.-.--   ";
-    std::cout<<Morse_to_text(test_morse_1, dict_morse_car)<<std::endl;
-    std::string test_text_1 = "     hello world !      ";
-    std::cout<<text_to_Morse(test_text_1, dict_car_morse)<<std::endl;
+    std::cout<<"here\n";
+    std::u32string test_morse_1 = U"    .... . .-.. .-.. ---   .-- --- .-. .-.. -..   -.-.--   ";
+    std::cout<<Morse_to_text(test_morse_1, dict_morse_car).c_str()<<std::endl;
+    std::u32string test_text_1 = U"     hello world !      ";
+    std::cout<<text_to_Morse(test_text_1, dict_car_morse).c_str()<<std::endl;
 
     if (argc >1){
         std::cout<<std::endl<<detect_entry_type(entry_text)<<std::endl;
