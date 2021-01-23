3cf6005d598e17e0a73351857b3a06ff09bd3e7a
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-01-23 15:22:37 +0100

    include file for to_audio.cpp (#3)

---
+++ to_audio.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<string>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+
+template <typename Word>
+std::ostream& write_word( std::ostream& outs, Word value, unsigned size);
+size_t create_file(std::ostream& f);
+void dash_wav(std::ofstream& f);
+void dot_wav(std::ofstream& f);
+void short_space_wav(std::ofstream& f);
+void space_wav(std::ofstream& f);
+void finish_file(std::ofstream& f, size_t data_chunk_pos);
