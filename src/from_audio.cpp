65fa6b6429aa8075cdeeace1f97e4523d68220bf
Louis-Justin TALLOT <lj.tallot@gmail.com>
2021-01-24 20:54:04 +0100

    scrape data from file (#3)

--- src/from_audio.cpp
+++ src/from_audio.cpp
@@ -7,3 +7,23 @@
 
 // g++ from_audio.cpp -g -o from_audio
 
+
+std::vector<int> file_data(std::ifstream& f){
+    std::streampos len;
+    f.seekg (0, std::ios::end);
+    len = f.tellg(); // this is the length of the file
+
+    std::vector<int> res;
+    int c;
+    
+    // we start from 48 to get straight to the data part 
+    // (by skipping the header)
+    f.seekg(48, std::ios::beg);
+
+    for (int i=0; i<int(len)-48; i++){
+        c = f.get(); //-> it is an integer
+        res.push_back(c);
+    }
+
+    return res;
+}
